4f480a3531acf6daaba02082390048f663568803
francisco8628 <francisco.ableite@gmail.com>
2020-04-17 01:50:50 -0300

    Single Connection

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,6 +1,7 @@
 
 <%@page import="beans.BeanCursoJsp"%>
 <jsp:useBean id="calcula" class="beans.BeanCursoJsp"	type="beans.BeanCursoJsp" scope="page" />
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix="c" %>
 	
 
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
@@ -12,7 +13,14 @@
 <title>Insert title here</title>
 </head>
 <body>
+       <c:set var = "data"  scope="page" value = "${500*20}"/>
+       
+       <c:out value="${data}"></c:out>
 
+<p/>
+<p/>
+<p/>
+<p/>
 	<form action="LoginServelet" method="post">
 
 		Login:<%-- é o label da pagina --%>
---
+++ src/connection/SingleConnection.java
@@ -0,0 +1,44 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnection {
+	private static String url = "jdbc:postgresql://localhost:5432/posjava";// url para conexao padrao do POSTGRES
+	private static String password = "admin";
+	private static String user = "postgres";
+	private static Connection conection = null; // tem que ser do pacote SQL java
+
+	static {
+
+		conectar();
+	}
+
+	public  SingleConnection() {
+ 	
+ 	conectar();
+ }
+
+	private static void conectar() {
+		try {
+			if (conection == null) {
+				Class.forName("org.postgresql.Driver");
+				conection = DriverManager.getConnection(url, user, password);
+				conection.setAutoCommit(false);// setar falso para nao salvar automatico
+				System.out.println("Conexão OK");// para teste da conexão
+			} else {
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static Connection getConnection() { // metodo que retorna a conexão
+
+		return conection;
+	}
+
+}
